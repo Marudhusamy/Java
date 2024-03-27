5a9689ac502d4c832c0469ba2fe73610b3b7e053
Marudhusamy <marudhusamy99@gmail.com>
2024-03-27 09:45:07 -0400

    calc package

---
+++ Calc.java
@@ -0,0 +1,20 @@
+package learn.basic;
+public class Calc
+{
+void add()
+{
+System.out.println(10+20);
+}
+void subtract()
+{
+System.out.println(10-20);
+}
+void multiply()
+{
+System.out.println(10+20);
+}
+void divate()
+{
+System.out.println(100/20);
+}
+}
