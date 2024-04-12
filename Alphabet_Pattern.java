88fac9ed9f20cf70558d141334e9eaf922d51a19
Marudhusamy <marudhusamy99@gmail.com>
2024-04-11 22:53:18 -0400

    Alphabet Task Programs

--- Alphabet_Pattern.java
+++ Alphabet_Pattern.java
@@ -17,6 +17,14 @@ public class Alphabet_Pattern {
 		p.print_Z();
 		p.print_X();
 		p.print_Y();
+		p.print_O();
+		p.print_U();
+		p.print_M();
+		p.print_W();
+		p.print_H();
+		p.print_A();
+		
+
 		
 		
 		
@@ -24,6 +32,140 @@ public class Alphabet_Pattern {
 		
 		
 
+	}
+
+	private void print_A() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=7;row++)
+		{
+			for(int col=1;col<=7;col++)
+			{
+				if(row==1)
+				{
+					if(col==1 || col==7)
+						System.out.print("  ");
+						else
+							System.out.print("* ");
+					}
+				else
+				{
+					if(col==1 || col==7 || row==5)
+					System.out.print("* ");
+					else
+						System.out.print("  ");
+				}
+				
+			}
+			System.out.println();
+		}
+		
+	}
+
+	private void print_H() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=7;row++)
+		{
+			for(int col=1;col<=7;col++)
+			{
+				if(col==1 || col==7 || row==4)
+				System.out.print("* ");
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+	private void print_W() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=7; row++)
+		{
+			for(int col=1;col<=14;col++)
+			{
+				if((row==col && col<=4) || (row+col==8 && row<=4))
+					  System.out.print("* ");   
+				else if (col-row==7 && col<=11 || row+col==15 && row<=4)
+					   System.out.print("* ");
+					   else 
+						   System.out.print("  ");
+					
+					
+			
+					}
+			System.out.println();
+		}
+		
+		
+	}
+
+	private void print_M() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=7;row++)
+		{
+			for(int col=1;col<=7;col++)
+			{
+				if(col==1 || col==7)
+				System.out.print("* ");
+				else if((row==col && col<=4) ||(row+col==8 && row<=4))
+					System.out.print("* ");
+				else 
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+	private void print_U() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=7;row++)
+		{
+			for(int col=1;col<=7;col++)
+			{
+				if(col==1 || col==7 || row==7)
+				{
+					if((col==1 && row==7)|| (col==7 && row==7))
+						System.out.print("  ");
+					else
+						System.out.print("* ");
+				}
+				
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+	private void print_O() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=7;row++)
+		{
+			for(int col=1;col<=7;col++)
+			{
+				if(row==1 || row==7)
+				{
+				   if(col==1 || col==7)
+					   System.out.print("  ");
+				   else
+					   System.out.print("* ");
+				}
+				else
+				{
+					if(col==1 || col==7)
+						System.out.print("* ");
+					else
+						System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		
 	}
 
 	private void print_Y() {
